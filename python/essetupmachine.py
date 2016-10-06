899bfc33bd0e2b70069bbd3b346140f71c05ad52
Srecko Morovic <srecko.morovic@cern.ch>
2016-10-06 11:33:53 +0200

    auto create parameter in elasticsearch.yml allows auto-creation of .marvel* indices

--- python/essetupmachine.py
+++ python/essetupmachine.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
               escfg.reg('discovery.zen.ping.unicast.hosts',json.dumps(es_cdaq_list))
               escfg.reg('discovery.zen.minimum_master_nodes','3')
             escfg.reg('discovery.zen.ping.multicast.enabled','false')
-            escfg.reg('action.auto_create_index','false')
+            escfg.reg('action.auto_create_index','.marvel-*')
             escfg.reg('index.mapper.dynamic','false')
             escfg.reg('transport.tcp.compress','true')
             escfg.reg('script.groovy.sandbox.enabled','true')
