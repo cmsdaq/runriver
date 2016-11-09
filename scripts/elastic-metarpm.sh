528e7e6bcb046b7dfdb2514754b74fd34972ddcc
Srecko Morovic <srecko.morovic@cern.ch>
2016-11-09 16:50:09 +0100

    new version number

--- scripts/elastic-metarpm.sh
+++ scripts/elastic-metarpm.sh
@@ -87,7 +87,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > fffmeta-elastic.spec <<EOF
 Name: $PACKAGENAME
-Version: 1.9.7
+Version: 1.9.8
 Release: 0
 Summary: hlt daemon
 License: gpl
