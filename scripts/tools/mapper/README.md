f86c83f29f42beec375fce0f007ea74a059c939f
Srecko Morovic <srecko.morovic@cern.ch>
2019-01-29 17:56:53 +0100

    fixing bootstrap scripts

--- scripts/tools/mapper/README.md
+++ scripts/tools/mapper/README.md
@@ -1,6 +1,7 @@
 Instructions for rolling over to new indices. Indices created: runindex, boxinfo, hltdlogs and merging
-1. Fetch latest mappings.py and updatemappings.py from hltd git master branch (python/)
-2. retrieve or comple latest river-runriver jar package, available in this repository (used in es-cdaq hosts) and link it to the river.jar name
-3. run makeIndicesCdaq.sh (or equivalent for system being rolled). A new set of indices will be created with default settings and mapping, but not yet active until [4]. The script will suggest to follow up with other command.
+1. Fetch latest mappings.py from hltd master branch. updatemappings.py (/opt/fff) might be also necessary if newer than provided is needed.
+2. find, retrieve or comple latest river-runriver jar package, available in this repository (used in es-cdaq hosts in /opt/fff) and link to it as river.jar
+3. run makeIndicesCdaq.sh (or equivalent for system concerned). A new set of indices will be created with default settings and mapping, but not yet active until [4]. The script will suggest to follow up with other command.
 4. After making sure no errors appeared previously, run the suggested update alias command, or save it for later. This should NOT be executed during an ongoing run!
+5. makeLustreIndex.sh can be run if necessary. See /opt/fff/setup_river_index.py to create river index
 
--- scripts/tools/mapper/makeIndicesMinidaq.sh
+++ scripts/tools/mapper/makeIndicesMinidaq.sh
@@ -11,19 +11,19 @@ year=${newdate:0:4}   ## or put explicit year if newdate doesn't start with year
 
 #make indices
 echo "make ${subsystem}_${newdate}"
-curl -XPUT es-cdaq:9200/runindex_${subsystem}_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "2","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
-curl -XPUT es-cdaq:9200/merging_${subsystem}_${newdate}?pretty  -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
-curl -XPUT es-cdaq:9200/boxinfo_${subsystem}_${newdate}?pretty  -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
-curl -XPUT es-cdaq:9200/hltdlogs_${subsystem}_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "2","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis":{"analyzer":{"prefix-test-analyzer":{"type":"custom","tokenizer":"prefix-test-tokenizer"}},"tokenizer":{"prefix-test-tokenizer":{"type": "path_hierarchy","delimiter":" "}}}}}'
+curl -H "Content-Type:application/json" -XPUT localhost:9200/runindex_${subsystem}_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "2","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
+curl -H "Content-Type:application/json" -XPUT localhost:9200/merging_${subsystem}_${newdate}?pretty  -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
+curl -H "Content-Type:application/json" -XPUT localhost:9200/boxinfo_${subsystem}_${newdate}?pretty  -d'{"settings":{"index":{"number_of_shards" : "8","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
+curl -H "Content-Type:application/json" -XPUT localhost:9200/hltdlogs_${subsystem}_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "2","number_of_replicas" : "1","codec" : "best_compression"},"mapper" : { "dynamic" : "false"}, "translog" : {"durability" : "async","flush_threshold_size":"4g"},"analysis":{"analyzer":{"prefix-test-analyzer":{"type":"custom","tokenizer":"prefix-test-tokenizer"}},"tokenizer":{"prefix-test-tokenizer":{"type": "path_hierarchy","delimiter":" "}}}}}'
 
 #run river jar with mapping as first parameter. Last parameter should specify name of the index, to inject the mapping.
 echo
 echo "#executing inject mapping:"
-/usr/bin/java -jar river.jar mapping ${subsystem} es-cdaq es-cdaq runindex_${subsystem}_${newdate} 0
-/opt/fff/updatemappings.py es-cdaq auto runindex_${subsystem}_${newdate} boxinfo_${subsystem}_${newdate} hltdlogs_${subsystem}_${newdate}
-/opt/fff/updatemappings.py es-cdaq copy runindex_${subsystem}_${newdate} merging_${subsystem}_${newdate}
+/usr/bin/java -jar river.jar mapping ${subsystem} localhost es-cdaq runindex_${subsystem}_${newdate} 0
+/opt/fff/updatemappings.py localhost auto runindex_${subsystem}_${newdate} boxinfo_${subsystem}_${newdate} hltdlogs_${subsystem}_${newdate}
+/opt/fff/updatemappings.py localhost copy runindex_${subsystem}_${newdate} merging_${subsystem}_${newdate}
 echo
 echo "#add set up aliases:"
-echo /opt/fff/updatemappings.py es-cdaq aliases ${subsystem} runindex_${subsystem}_${newdate} boxinfo_${subsystem}_${newdate} hltdlogs_${subsystem}_${newdate} merging_${subsystem}_${newdate} $year
+echo /opt/fff/updatemappings.py localhost aliases ${subsystem} runindex_${subsystem}_${newdate} boxinfo_${subsystem}_${newdate} hltdlogs_${subsystem}_${newdate} merging_${subsystem}_${newdate} $year
 
 #TODO: add year alias mapping (like runindex_minidaq2017_read)
