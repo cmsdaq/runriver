7833625e562592a320e11a1ab8532a26c5e84b17
Srecko Morovic <srecko.morovic@cern.ch>
2019-02-04 14:10:48 +0100

    fix parameter for elasticsearch server

--- scripts/elastic-metarpm.sh
+++ scripts/elastic-metarpm.sh
@@ -83,7 +83,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > fffmeta-elastic.spec <<EOF
 Name: $PACKAGENAME
-Version: 2.5.0
+Version: 2.5.1
 Release: 0
 Summary: hlt daemon
 License: gpl
