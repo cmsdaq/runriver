60664b12ffe65f08db164b53aba90384d0c4658f
Srecko Morovic <srecko.morovic@cern.ch>
2019-08-21 12:01:01 +0200

    add mapping to lustre_info (forcing keyword instead of text). version update

--- scripts/tools/mapper/makeLustreIndex.sh
+++ scripts/tools/mapper/makeLustreIndex.sh
@@ -7,9 +7,9 @@ cluster="localhost"
 
 #make indices
 echo "make lustre_info_${newdate}"
-curl -H "Content-Type:application/json" -XPUT ${cluster}:9200/lustre_info_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "4","number_of_replicas" : "1","codec" : "best_compression"}, "translog" : {"durability" : "async"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}}}'
+curl -H "Content-Type:application/json" -XPUT ${cluster}:9200/lustre_info_${newdate}?pretty -d'{"settings":{"index":{"number_of_shards" : "4","number_of_replicas" : "1","codec" : "best_compression"}, "translog" : {"durability" : "async"},"analysis" : {"analyzer" : {"default" : {"type" : "keyword"}}}},"mappings":{"properties":{"host":{"type":"keyword"},"doc_type":{"type":"keyword"}  }}}'
 
 echo "IF OK RUN THIS:"
-echo curl -H "Content-Type:application/json" -XPOST ${cluster}:9200/_aliases -d'{"actions":[{"remove":{"index":"lustre_info_*","alias":"lustre_info"}},{"add":{"index":"lustre_info_'${newdate}'","alias":"lustre_info"}}]}'
+echo curl -H "'Content-Type:application/json'" -XPOST ${cluster}:9200/_aliases -d"'"'{"actions":[{"remove":{"index":"lustre_info_*","alias":"lustre_info"}},{"add":{"index":"lustre_info_'${newdate}'","alias":"lustre_info"}}]}'"'"
 
 #TODO: add year alias mapping (like runindex_minidaq2017_read)
